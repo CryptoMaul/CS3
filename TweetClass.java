dfbbb2f144166f25188f762f4196616b635895ab
CryptoMaul <99741455+CryptoMaul@users.noreply.github.com>
2023-08-18 13:27:06 +0000

    TweetClass

---
+++ TweetClass.java
@@ -0,0 +1,38 @@
+public class TweetClass{
+    private String message;
+    private int retweets;
+    private int likes;
+
+    public Tweet(String m, int r, int l){
+        message = m;
+        retweets = r;
+        likes = l;
+    }
+    public void addLikes(int l){
+        likes += l;
+    }
+    public void addRetweets(int r){
+        retweets += r;
+    }
+    public boolean notLiked(){
+        if(likes < 10){
+            return true;
+        }
+        return false;
+    }
+    public boolean kindaLiked(){
+        if(likes > retweets){
+            return true;
+        }
+        return false;
+    }
+    public boolean isTrending(){
+        if(likes + retweets > 75){
+            return true;
+        }
+        return false;
+    }
+    public String toString(){
+        return "Message: " + message + ", Likes: " + likes + ", Retweets: " + retweets; 
+    }
+}
